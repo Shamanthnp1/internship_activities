07e06796b491b6e44f6f2bd0777d8531bd496261
Shamanthnp1 <shamanthnp@gmail.com>
2026-02-19 12:46:38 +0530

    day_16 added

--- SRC/day16/day16_task3.py
+++ SRC/day16/day16_task3.py
@@ -1,16 +1,27 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = np.random.exponential(scale=2, size=10000)
+np.random.seed(42)
+original_data = np.random.exponential(scale=2, size=10000)
+
+plt.figure(figsize=(12,4))
+plt.subplot(1,2,1)
+sns.histplot(original_data, kde=True)
+plt.title("Original Data (Right-Skewed)")
 
 sample_means = []
-
-for _ in range(1000):
-    sample = np.random.choice(data, size=30)
+for i in range(1000):
+    sample = np.random.choice(original_data, size=30)
     sample_means.append(np.mean(sample))
 
-plt.hist(sample_means, bins=30)
-plt.title("Distribution of Sample Means (CLT)")
-plt.xlabel("Sample Mean")
-plt.ylabel("Frequency")
+sample_means = pd.Series(sample_means)
+plt.subplot(1,2,2)
+sns.histplot(sample_means, kde=True)
+plt.title("Distribution of Sample Means (n=30)")
+plt.tight_layout()
 plt.show()
+print("\nOriginal Data Mean:", np.mean(original_data))
+print("\nMean of Sample Means:", np.mean(sample_means))
+print("\nStandard Deviation of Sample Means:", np.std(sample_means))
